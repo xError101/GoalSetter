ef042545e2474da6bcee645dee211a8caaa05c00
xError101 <error.not.known.404@gmail.com>
2024-09-08 16:23:16 +0100

    Added frontend UI

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const {getGoals,setGoal, updateGoal, deleteGoals} = require('../controllers/goalController')
 
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoals)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoals)
 
 module.exports = router
